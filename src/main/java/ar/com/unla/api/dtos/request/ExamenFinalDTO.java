2e48cabe79b771a63c267df875d27f64e50d9d6e
Gianni-Nicolas <nicolas.gianni@wolox.com.ar>
2020-11-14 15:39:44 -0300

    date validations are deleted in registration periods and final exams

--- src/main/java/ar/com/unla/api/dtos/request/ExamenFinalDTO.java
+++ src/main/java/ar/com/unla/api/dtos/request/ExamenFinalDTO.java
@@ -3,7 +3,6 @@ package ar.com.unla.api.dtos.request;
 import ar.com.unla.api.constants.CommonsErrorConstants;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,6 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 public class ExamenFinalDTO {
 
     @NotNull(message = CommonsErrorConstants.REQUIRED_PARAM_ERROR_MESSAGE)
-    @Future(message = CommonsErrorConstants.FUTURE_DATE_ERROR_MESSAGE)
     @DateTimeFormat(iso = ISO.DATE, pattern = "uuuu-MM-dd")
     @ApiModelProperty(required = true, position = 1)
     private LocalDate fecha;
